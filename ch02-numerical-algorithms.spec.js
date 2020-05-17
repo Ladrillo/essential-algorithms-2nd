75cf61b6b9f5fccf9147ba93353f45d99ff77aa7
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-05-17 11:41:04 +0200

    bah

--- ch02-numerical-algorithms.spec.js
+++ ch02-numerical-algorithms.spec.js
@@ -28,9 +28,11 @@ it('returns random numbers', () => {
   expect(gen.next().value).toBe(0) // cycle starting again
 })
 
-// ENSURING FAIRNESS
-// PRNG can be biased or fair.
+// ENSURING FAIRNESS - PRNG can be biased or fair.
+
 // 1- using a linear congruential generator for a specific range
+
+// 1A- The bad approach
 function* badRangeRandomGenerator(min, max) {
   const gen = makeRandom(7, 5, 11)
 
@@ -39,15 +41,34 @@ function* badRangeRandomGenerator(min, max) {
     yield min + number % (max - min + 1)
   }
 }
-const get = badRangeRandomGenerator(0, 3)
-console.log(get.next().value)
-console.log(get.next().value)
-console.log(get.next().value)
-console.log(get.next().value)
-console.log(get.next().value)
-console.log(get.next().value)
-console.log(get.next().value)
-console.log(get.next().value)
-console.log(get.next().value)
-console.log(get.next().value)
-console.log('cycle restarts')
+it('returns biased random numbers either 1 or 2', () => {
+  const gen = badRangeRandomGenerator(1, 2)
+
+  expect(gen.next().value).toBe(1)
+  expect(gen.next().value).toBe(2)
+  expect(gen.next().value).toBe(2)
+})
+
+// 1B- A better approach
+function* betterRangeGenerator(min, max) { // Not working
+  const gen = makeRandom(7, 5, 11)
+
+  while (true) {
+    const number = gen.next().value
+    yield min + (number / 11) * (max - min)
+  }
+}
+it('returns non-biased random numbers between 1 and 2', () => {
+  const gen = betterRangeGenerator(0, 2)
+
+  console.log(gen.next().value)
+  console.log(gen.next().value)
+  console.log(gen.next().value)
+  console.log(gen.next().value)
+  console.log(gen.next().value)
+  console.log(gen.next().value)
+  console.log(gen.next().value)
+  console.log(gen.next().value)
+  console.log(gen.next().value)
+  console.log(gen.next().value)
+})
