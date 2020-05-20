437168873ec9c66c1bd1f9b09cc325d767896f38
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-05-20 11:47:33 +0200

    random walk

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,8 @@ module.exports = {
     "env": {
         "es6": true,
         "node": true,
-        "jest": true
+        "jest": true,
+        "browser": true
     },
     "extends": "eslint:recommended",
     "globals": {
---
+++ ch02-numerical-algorithms.js
@@ -0,0 +1,40 @@
+// 8- RANDOM WALKS
+function makeRandoWalk(numPoints, canvasSize, stepSize) {
+  let x = Math.round(canvasSize / 2)
+  let y = Math.round(canvasSize / 2)
+
+  let listOfPoints = []
+
+  listOfPoints.push([x, y])
+
+  for (let i = 1; i < numPoints; i++) {
+    const direction = Math.floor(Math.random() * 4)
+    if (direction === 0) y -= stepSize      // up
+    else if (direction === 1) x += stepSize // right
+    else if (direction === 2) y += stepSize // down
+    else x -= stepSize                      // left
+
+    listOfPoints.push([x, y])
+  }
+
+  return listOfPoints
+}
+
+{
+  const numPoints = 200
+  const canvasSize = 400
+  const stepSize = 20
+
+  const canvas = document.createElement('canvas')
+  canvas.width = canvasSize
+  canvas.height = canvasSize
+  canvas.style.border = '1px solid black'
+  const ctx = canvas.getContext('2d')
+  const points = makeRandoWalk(numPoints, canvasSize, stepSize)
+  for (let idx = 0; idx < points.length - 1; idx++) {
+    ctx.moveTo(...points[idx])
+    ctx.lineTo(...points[idx + 1])
+  }
+  ctx.stroke()
+  document.body.appendChild(canvas)
+}
