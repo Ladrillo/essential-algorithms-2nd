551ba2d0d2adfa58174557c15a04477f9b39c34a
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-05-28 12:05:57 +0200

    fermat's little theorem

--- ch02-numerical-algorithms.spec.js
+++ ch02-numerical-algorithms.spec.js
@@ -1,3 +1,4 @@
+/* global BigInt */
 // 1- CREATING PSEUDORANDOM NUMBERS
 
 // using a linear congruential generator
@@ -52,7 +53,8 @@ it('biasedRangeRandomNumGen either 1 or 2', () => {
 })
 
 // B better approach
-// Makes floats between min and max ???
+// Makes floats between min and max ?????????????
+// eslint-disable-next-line
 function* betterRangeGenerator(min, max) {
   const gen = makeRandom(7, 5, 11)
 
@@ -338,4 +340,43 @@ it('findPrime using the sieve', () => {
   ]
   expect(findPrimesWithEratosthenesSieve(10)).toEqual(primesUpToTen)
   expect(findPrimesWithEratosthenesSieve(100)).toEqual(primesUpTo100)
+})
+
+// 12- TESTING FOR PRIMALITY
+function getRandomIntBetween(min, max) { // min inclusive; max exclusive
+  return Math.floor(Math.random() * (max - min) + min)
+}
+
+function isPrimeFermat(number, numberTests) {
+  const tests = [...Array(numberTests).keys()]
+  // eslint-disable-next-line
+  for (let _ in tests) {
+    // annoying big int preparations
+    const candidate = BigInt(number)
+    const witness = BigInt(getRandomIntBetween(1, number))
+    // fermat's little theorem
+    const notPrime = (witness ** (candidate - 1n)) % candidate !== 1n
+    if (notPrime) return false
+  }
+  return true
+}
+it('isPrimeFermat', () => {
+  const primesUpTo100 = [
+    2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41,
+    43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97,
+  ]
+  const compositesUpTo100 = [
+    4, 6, 8, 9, 10, 12, 14, 15, 16, 18, 20, 21, 22, 24,
+    25, 26, 27, 28, 30, 32, 33, 34, 35, 36, 38, 39, 40,
+    42, 44, 45, 46, 48, 49, 50, 51, 52, 54, 55, 56, 57,
+    58, 60, 62, 63, 64, 65, 66, 68, 69, 70, 72, 74, 75,
+    76, 77, 78, 80, 81, 82, 84, 85, 86, 87, 88, 90, 91,
+    92, 93, 94, 95, 96, 98, 99, 100,
+  ]
+  primesUpTo100.forEach(prime => {
+    expect(isPrimeFermat(prime, 5)).toBe(true)
+  })
+  compositesUpTo100.forEach(composite => {
+    expect(isPrimeFermat(composite, 5)).toBe(false)
+  })
 })
