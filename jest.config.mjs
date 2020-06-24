e2dde967164b184a3c80f63da03c924e00213e49
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-06-24 12:54:48 +0200

    remove unneeded jest config tweak

--- jest.config.mjs
+++ jest.config.mjs
@@ -164,9 +164,7 @@ export default {
   // timers: "real",
 
   // A map from regular expressions to paths to transformers
-  transform: {
-    "^.+\\.jsx?$": "babel-jest",
-  },
+  // transform: undefined,
 
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
   // transformIgnorePatterns: [
